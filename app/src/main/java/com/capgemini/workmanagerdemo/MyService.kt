98adeaf588be107bdeb04be1d7a7ac98a717af6f
shridevi-sawant <shridevi.hiroji@gmail.com>
2021-07-13 16:12:28 +0530

    Service - added onCreate with log

--- app/src/main/java/com/capgemini/workmanagerdemo/MyService.kt
+++ app/src/main/java/com/capgemini/workmanagerdemo/MyService.kt
@@ -3,10 +3,16 @@ package com.capgemini.workmanagerdemo
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class MyService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("MyService", "test")
     }
 }
